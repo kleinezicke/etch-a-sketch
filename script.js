cd6da6bb051298919f7fe4f67857422ce6e383f3
kleinezicke <julia.neuburger@24speed.at>
2021-11-12 15:55:09 +0100

    added button to make a new grid of a size between 1 and 100

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
 const gridContainer = document.querySelector("#grid");
-
-function makeGrid(boxSize) {
-
-    let boxSide = 600 / boxSize;
-
-    for (let i = 0; i < boxSize * boxSize; i++)
-    {
-        let box = document.createElement("div")
-        box.classList.add("box")
-        box.style.height = boxSide + "px";
-        box.style.width = boxSide + "px";
-        box.addEventListener ('mouseover', function(e) {
-            box.style.backgroundColor = "black";
-        })
-        gridContainer.appendChild(box)
-    }
-}
+const resetBtn = document.querySelector("#reset")
+const newBtn = document.querySelector("#new");
 
 gridContainer.onload = makeGrid(16);
 
-const resetBtn = document.querySelector("#reset")
 
 resetBtn.addEventListener("click", function(e) {
 
@@ -31,3 +15,37 @@ resetBtn.addEventListener("click", function(e) {
     }
 
 })
+
+newBtn.addEventListener("click", function(e) {
+    removeGrid();
+    makeGrid((prompt("How big should the new grid be?", "16")))
+})
+
+function makeGrid(boxSize) {
+
+    let boxSide = 600 / boxSize;
+
+    if (boxSize <= 100 && boxSize > 0) {
+        
+        for (let i = 0; i < boxSize * boxSize; i++)
+        {
+            let box = document.createElement("div")
+            box.classList.add("box")
+            box.style.height = boxSide + "px";
+            box.style.width = boxSide + "px";
+            box.addEventListener ('mouseover', function(e) {
+                box.style.backgroundColor = "black";
+            })
+            gridContainer.appendChild(box)
+        }    }
+
+}
+
+function removeGrid() {
+
+    let boxes = gridContainer.querySelectorAll(".box")
+    boxes.forEach(element => {
+        gridContainer.removeChild(element)
+    });
+
+}
